066c3cc26af1ed973768d48db9b7df68cef7fe2e
张翔 <zhangxiang@shwoody.com>
2025-11-06 11:20:32 +0800

    fix: 调整本地图片路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,15 @@ export default defineConfig({
   integrations: [tailwind()],
 
   // dev mode
-  site: 'http://localhost:4321',
+  site: 'http://172.17.1.210:4321',
 
   // 允许反向代理域名（如 Cloudflare Tunnel 的 *.trycloudflare.com）访问开发服务器
   // 也可改为数组：allowedHosts: ['localhost', '10.10.16.59', '*.trycloudflare.com']
-  // vite: {
-  //   server: {
-  //     allowedHosts: true,
-  //   }
-  // },
+  vite: {
+    server: {
+      allowedHosts: ['localhost', '10.10.16.59', 'attract-diego-corrected-organisations.trycloudflare.com'],
+    }
+  },
 
   // production mode
   
