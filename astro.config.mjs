b21d7f0e60334211d56d5a9fd68f0e74d842a86c
张翔 <zhangxiang@shwoody.com>
2026-01-22 16:33:07 +0800

    feat: site针对dev和prd使用不同域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { loadEnv } from "vite";
 
-const { ASTRO_SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { ASTRO_SITE, ASTRO_SITE_DEV } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
 
-  // dev mode
-  site: ASTRO_SITE,
+  // 根据当前环境决定使用哪个域名
+  // 开发环境使用 localhost，生产环境使用正式域名
+  site: process.env.NODE_ENV === "development" ? ASTRO_SITE_DEV : ASTRO_SITE,
 
   // 允许反向代理域名（如 Cloudflare Tunnel 的 *.trycloudflare.com）访问开发服务器
   // 也可改为数组：allowedHosts: ['localhost', '10.10.16.59', '*.trycloudflare.com']
